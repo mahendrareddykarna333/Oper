7fc4867d4ee1d40fed5c6f60261b7f3354939135
Mahendra Reddy Karna <karna.mailcity@gmail.com>
2021-09-28 09:59:40 +0100

    Initial commit

---
+++ src/Operators.java
@@ -0,0 +1,16 @@
+public class Operators
+{ public  static void main(String ar[])
+{
+    byte x=4;
+    byte y=2;
+
+
+    System.out.println("result of addition is;"+(x+y));
+    System.out.println("result of sub is;" +(x-y));
+    System.out.println("result of mul is;"+(x*y));
+    System.out.println("result of div is;"+(x/y));
+    System.out.println("result of mod is;"+(x%y));
+
+
+}
+}
